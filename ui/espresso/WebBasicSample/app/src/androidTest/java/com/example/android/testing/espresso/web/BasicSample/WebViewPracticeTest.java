151f942508ca74d78654412fcaa9f7c53ba970e7
Gaurav Singh <gauravsingh1@fb.com>
2022-05-16 14:46:47 +0100

    - Added first web espresso test

--- ui/espresso/WebBasicSample/app/src/androidTest/java/com/example/android/testing/espresso/web/BasicSample/WebViewPracticeTest.java
+++ ui/espresso/WebBasicSample/app/src/androidTest/java/com/example/android/testing/espresso/web/BasicSample/WebViewPracticeTest.java
@@ -1,10 +1,22 @@
 package com.example.android.testing.espresso.web.BasicSample;
 
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.web.assertion.WebViewAssertions.webMatches;
 import static androidx.test.espresso.web.sugar.Web.onWebView;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.clearElement;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.findElement;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.getText;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.webClick;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.webKeys;
+import static org.hamcrest.Matchers.containsString;
 
+import android.content.Intent;
+
+import androidx.test.espresso.web.webdriver.Locator;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,8 +32,28 @@ public class WebViewPracticeTest {
         }
     };
 
+    @Before
+    public void launchActivity() {
+        activityScenarioRule.launchActivity(withWebFormIntent());
+    }
+
+    public Intent withWebFormIntent() {
+        Intent basicFormIntent = new Intent();
+        basicFormIntent.putExtra(WebViewActivity.KEY_URL_TO_LOAD, WebViewActivity.WEB_FORM_URL);
+        return basicFormIntent;
+    }
+
     @Test
     public void whenUserEntersText_AndTapsOnChange_ThenTextIsChangedInANewPage() {
+        String text = "Peekaboo";
+        onWebView(withId(R.id.web_view))
+                .withElement(findElement(Locator.ID, "text_input"))
+                .perform(clearElement())
+                .perform(webKeys(text))
+                .withElement(findElement(Locator.ID, "submitBtn"))
+                .perform(webClick())
+                .withElement(findElement(Locator.ID, "response"))
+                .check(webMatches(getText(), containsString(text)));
 
     }
 }
